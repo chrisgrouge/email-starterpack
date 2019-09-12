38e102e1794116484f2882c52e5a8a62097c2a84
Hunter <jenningsdhunter@gmail.com>
2019-09-12 16:45:03 -0400

    Major changes here:
    	- Upgraded gulp version and gulp script
    	- completely rewrote the architecture
    		- file organization and directories
    	- gulp script now creates a single `index.html` file while also providing an option to separate out the modules into their own `html` files
    
    TODO:
    
    	- Review with team and add more 'generic' modules where necessary

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,163 @@
-var gulp = require("gulp");
-var gulpInline = require("gulp-inline-css");
-var browserSync = require("browser-sync").create();
-var imagemin = require("gulp-imagemin");
+const {
+  series,
+  src,
+  dest,
+  watch,
+  parallel
+} = require("gulp");
+const gulpInline = require("gulp-inline-css");
+const sass = require('gulp-sass');
+const nunjucksRender = require("gulp-nunjucks-render");
+const htmlmin = require('gulp-html-minifier');
+const dom = require('gulp-dom');
+const browserSync = require("browser-sync");
+const server = browserSync.create();
+
+sass.compiler = require('node-sass');
 
 
-//Image compression
-gulp.task("imagemin", function() {
-  gulp
-    .src("src/assets/*")
-    .pipe(imagemin())
-    .pipe(gulp.dest('dist/assets'));
-});
 
-
-// Automatic Inlining
-gulp.task("gulpInline", function() {
-  gulp
-    .src("src/build.html")
-    .pipe(gulpInline({ preserveMediaQueries: true, applyWidthAttributes: true }))
-    .pipe(gulp.dest("dist/build-inline"))
-    .pipe(
-      browserSync.reload({
-        stream: true
-      })
-    );
-});
-
-
-//Browser-syncing 
-gulp.task("browserSync", runSync);
-
-function runSync() {
-  setTimeout(function() {
-    browserSync.init({
-      server: {
-        baseDir: "src",
-        index: "build.html"
-      }
-    });
-  }, 500);
+function copy() {
+  return src('./src/assets/*.+(png|jpg|gif|jpeg)')
+    .pipe(dest('./dist/assets/'))
 }
 
-// Watchers
-gulp.task("watch", ["gulpInline", "browserSync", "imagemin"], function() {
-  gulp.watch(["src/build.html"], ["gulpInline"]);
-  gulp.watch(["src/css/*.css"], ["gulpInline"]);
-  gulp.watch(["src/assets/*"], ["imagemin"]);
-});
+/* 
 
-gulp.task("default", ["watch"]);
+ * Standard Nunjucks script:
+  * - Run when the default `gulp` script is run
+    - Looks for files in the './src/pages/' folder
+    - Runs it thorough the nunjucks function
+    - Inlines css
+    - and outputs the results here './dist/'
+    - streams changes to browser-sync instance invoked by the `gulp` script
+*/
+
+function nunjucks() {
+  return src('./src/pages/*.+(html|nunjucks|njk)')
+    .pipe(
+      nunjucksRender({
+        path: ["./src/templates"]
+      }))
+    .pipe(
+      gulpInline({
+        preserveMediaQueries: true,
+        applyWidthAttributes: true,
+        removeLinkTags: false,
+      }))
+    .pipe(dest('./dist'))
+    .pipe(browserSync.stream());
+}
+
+/* 
+
+ * Code Perfect Specific Nunjucks script:
+  - Looks for files in the './src/pages/modules/' folder
+  - Runs it thorough the nunjucks function
+  - Minifies the html
+  - and outputs the results here './dist/modules' 
+
+*/
+
+/* 
+
+* Function that loops through a node list of elements with a 
+* [data-type] attribute, and appends another [data="#"] attribute
+* with it's respective array index number.
+
+*/
+function getDataAttributes() {
+  this.querySelectorAll('[data-type]').forEach((cell, index) => {
+    const span = this.querySelector('span');
+    
+    if(span === null) return;
+    
+    const attr = span.getAttribute('data');
+    cell.setAttribute('data', `${attr}${index}`);
+  });
+  return this;
+}
+
+/* 
+
+* Function that creates individual `html` module files 
+* referenced in `src > pages > modules` and 
+* outputs them into the `dist > modules` folder.
+  ! No Attributes
+*/
+function runNunjucksNoAttributes() {
+  return src('./src/pages/modules/*.+(html|nunjucks|njk)')
+    .pipe(
+      nunjucksRender({
+        path: ["./src/templates"]
+      }))
+    .pipe(
+      gulpInline({
+        preserveMediaQueries: true,
+        applyWidthAttributes: true,
+        removeLinkTags: true,
+      }))
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(dest('./dist/modules'))
+}
+
+/* 
+
+* Function that creates individual `html` module files 
+* referenced in `src > pages > modules` and 
+* outputs them into the `dist > modules` folder.
+  ! With Attributes
+*/
+function runNunjucksWithAttributes() {
+  return src('./src/pages/modules/*.+(html|nunjucks|njk)')
+  .pipe(
+    nunjucksRender({
+      path: ["./src/templates"]
+    }))
+      .pipe(dom(getDataAttributes))
+      .pipe(htmlmin({
+        collapseWhitespace: true
+      }))
+    .pipe(dest('./dist/modules'))
+}
+
+// * Converts sass to css
+function sassFn() {
+  return src('./src/scss/styles.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('./src/css'))
+    .pipe(browserSync.stream());
+}
+
+// * Creates a browsersync instance
+function browser_sync(done) {
+  browserSync.init({
+    server: {
+      baseDir: "./dist",
+      index: "index.html"
+    }
+  });
+  done();
+}
+
+// * Watches for file changes in `src` folder
+function watchFiles() {
+  watch(
+    [
+      './src/scss/**/*.scss',
+      './src/**/*.+(html|nunjucks|njk)'
+    ], series(sassFn, nunjucks))
+  watch('./src/assets/*.+(png|jpg|gif|jpeg)', copy)
+}
+
+// Default gulp variable
+// * represents the `gulp` command
+const watchFn = series(sassFn, nunjucks, parallel(watchFiles, browser_sync));
+
+exports.default = watchFn;
+
+exports.copy = copy;
+exports.get_njk_modules = runNunjucksNoAttributes;
+exports.get_njk_modules_atr = runNunjucksWithAttributes;
